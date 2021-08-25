82b124120ed2e9b24cd0061b06ec705191381a3a
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-08-25 05:11:58 -0700

    Cleanse
    Don't be lazy 😉

--- src/Soulz/Honey/commands/enchants/HoneyEnchantPickaxe.php
+++ src/Soulz/Honey/commands/enchants/HoneyEnchantPickaxe.php
@@ -10,7 +10,7 @@ use pocketmine\item\enchantment\EnchantmentInstance;
 use pocketmine\item\Pickaxe;
 use pocketmine\network\mcpe\protocol\types\Enchant;
 use pocketmine\Player;
-use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\TextFormat;
 use pocketmine\utils\TextFormatJsonObject;
 use Soulz\Honey\Honey;
 use Soulz\Honey\Utils;
@@ -24,16 +24,16 @@ class HoneyEnchantPickaxe extends Command{
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): void{
         if(!$sender->isOp()){
-            $sender->sendMessage(TF::RED . "No permission.");
+            $sender->sendMessage(TextFormat::RED . "No permission.");
             return;
         }
         if(!$sender instanceof Player){
-            $sender->sendMessage(TF::RED . "Please use this command in-game");
+            $sender->sendMessage(TextFormat::RED . "Please use this command in-game");
             return;
         }
         $item = $sender->getInventory()->getItemInHand();
         if(!$item instanceof Pickaxe) {
-            $sender->sendMessage(TF::RED . "You have to be holding a pickaxe.");
+            $sender->sendMessage(TextFormat::RED . "You have to be holding a pickaxe.");
             return;
         }
         if(!isset($args[0])){
@@ -46,13 +46,13 @@ class HoneyEnchantPickaxe extends Command{
         foreach($lore as $key => $datum){
             if(strpos($datum, "Honey ") !== false){
                 unset($lore[$key]);
-                $lore[] = TF::RESET . TF::LIGHT_PURPLE . "Honey " . Utils::lvlToRomanNum($level);
+                $lore[] = TextFormat::RESET . TextFormat::LIGHT_PURPLE . "Honey " . Utils::lvlToRomanNum($level);
             }else{
-                $lore[] = TF::RESET . TF::LIGHT_PURPLE . "Honey " . Utils::lvlToRomanNum($level);
+                $lore[] = TextFormat::RESET . TextFormat::LIGHT_PURPLE . "Honey " . Utils::lvlToRomanNum($level);
             }
         }
         if(empty($lore)){
-            $lore[] = TF::RESET . TF::LIGHT_PURPLE . "Honey " . Utils::lvlToRomanNum($level);
+            $lore[] = TextFormat::RESET . TextFormat::LIGHT_PURPLE . "Honey " . Utils::lvlToRomanNum($level);
         }
         $item->setLore($lore);
         $sender->getInventory()->setItemInHand($item);
